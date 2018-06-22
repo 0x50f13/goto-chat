95562cdb2fde1a1e7b09d92491189e19effc699f
Alexey Kovba <alex.kovba@gmail.com>
2018-06-22 15:29:53 +0300

    encryptor
    added msg encr(PrK)

---
+++ net/encryptor.py
@@ -0,0 +1,9 @@
+import nacl
+def encryption(msg):
+    key = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
+    box = nacl.secret.SecretBox(key)
+    message = msg
+    encrypted = box.encrypt(message)
+    nonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
+    encrypted = box.encrypt(message, nonce)
+    return encrypted
